1e2388764952aee4165f3a46e86ccc49a9478105
DocLJN <ljnoble@gmail.com>
2018-03-10 11:56:24 +0000

    Removed duplication

--- client/src/models/location_map.js
+++ client/src/models/location_map.js
@@ -6,38 +6,6 @@ const LocationMap = function () {
   this.map = null;
 };
 
-LocationMap.prototype.create = function (container, countryCodeArray, seriesName) {
-  const mapDataObject = {
-    chart: {
-      map: worldMap,
-      borderWidth: 0
-    },
-
-    title: {
-      text: ""
-    },
-
-    subtitle: {
-      text: "Where you will be understood"
-    },
-
-    legend: {
-      enabled: false
-    },
-
-    series: [{
-      enableMouseTracking: false,
-      name: seriesName,
-      data: [
-        countryCodeArray
-      ]
-    }]
-  };
-
-  const map = Highcharts.mapChart(container, mapDataObject);
-  this.map = map;
-
-};
 
 LocationMap.prototype.createCommonLanguageCountries = function (container, countriesWhereTargetIsSpokenArray){
   const countryCodeArray = countriesWhereTargetIsSpokenArray.map( function (country) {
@@ -45,8 +13,8 @@ LocationMap.prototype.createCommonLanguageCountries = function (container, count
     const requiredData = [countryCode, 500];
     return requiredData;
   });
-  console.log(countryCodeArray);
-  const mapDataObject = {
+  const mapDataObject =
+  {
     chart: {
       map: worldMap,
       borderWidth: 0
@@ -68,7 +36,8 @@ LocationMap.prototype.createCommonLanguageCountries = function (container, count
         countryCodeArray
 
     }]
-  };
+  }
+  ;
   const map = Highcharts.mapChart(container, mapDataObject);
   this.map = map;
 };
