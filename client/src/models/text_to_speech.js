398274cd51e19301c69015893f84fa5bfdc72dfb
Olga Maunsell <olgamaunsell@yahoo.co.uk>
2018-03-12 19:22:28 +0000

    commented out else statement as it appears to change voices
    when used Argentina as an example speaking 2 different words - changing from male voice using voice of es-AR to a female voice with lang es-US

--- client/src/models/text_to_speech.js
+++ client/src/models/text_to_speech.js
@@ -30,31 +30,30 @@ TextToSpeech.prototype.speakPhrase = function(phrase, speechLanguage){
   speech.text = phrase;
   speech.lang = speechLanguage;
   console.log("speech.lang", speech.lang);
-  console.log("this.voices - speakPhrase 2", this.voices);
 
   this.voices.forEach(function(voice){
     console.log("voice.lang", voice.lang);
     console.log("speechLanguage", speechLanguage);
     if (voice.lang === speechLanguage){
       console.log("voice.lang", voice.lang);
-      console.log("speechLanguage", speechLanguage);
       speech.voice = voice;
       console.log("speech.voice 1", speech.voice);
       return;
     }
-    else{
-      let voiceLanguageCode = voice.lang.slice(0,2);
-      console.log("voiceLanguageCode", voiceLanguageCode);
-      if (voiceLanguageCode === selectedLanguageCode){
-        speech.voice = voice;
-        console.log("speech.voice 2", speech.voice);
-        return;
-      }
-    }
+    // else{
+    //   let voiceLanguageCode = voice.lang.slice(0,2);
+    //   console.log("voiceLanguageCode", voiceLanguageCode);
+    //   if (voiceLanguageCode === selectedLanguageCode){
+    //     speech.voice = voice;
+    //     console.log("speech.voice 2", speech.voice);
+    //     return;
+    //   }
+    // }
   });
   // ** needs further investigation as when it's needed to set .voice as speechSynthesis still
-  // works without setting .voice to meet the logic written above e.g. speechLanguage (es-BO) for Bolivia speak phrase works even though there is no voice for (es-BO) so speechSynthesis must be using a fall back of another 'es' speaking voice instead. For Serbia (sr-RS), there is no voice for this language but the word is spoken with an English voice.
-  // I assume for more complicated languages like Bengali (bn-BD) there is no voice as it's not possible for the English voice to pronounce the text ?
+  // works without setting speech.voice to meet the logic written above e.g. speechLanguage (es-BO) for Bolivia speak phrase works even though there is no voice for (es-BO) so speechSynthesis must be using a fall back of another 'es' speaking voice instead. For Serbia (sr-RS), there is no voice for this language but the word is spoken with an English voice.
+  // I assume for more complicated languages like Bengali (bn-BD) there is no voice as it's not possible for the English voice to pronounce the text ? so it appears we only need to set speech.lang.
+  // **
 
 console.log("speech voice - before speak", speech.voice);
 
